a5c6a3be2c0d4b460267fff2fc65e0b870263317
Verdes-C <verdesconstantinadrian@gmail.com>
2022-05-15 08:44:09 +0300

    nav, footer done + start ejs

--- node.js
+++ node.js
@@ -2,26 +2,21 @@
 //modules
 const fs = require('fs');
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
-app.use(express.static('error'));
-//routing
-// app.get('/', (req, res) => {
-//   console.log('am cerut');
-//   console.log(req);
-//   res.sendFile(path.join(__dirname, '/index.html'));
-// });
+app.use(express.static(`${__dirname}/static`));
+app.use(express.static(`${__dirname}/static/error`));
+app.use(express.static(`${__dirname}/static/css`));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
-app.get('/main.css', (req, res) => {
-  res.sendFile(`${__dirname}/main.css`);
+  res.render('game');
 });
 
 //routing to 404
 app.all('*', (req, res) => {
-  res.sendFile(`${__dirname}/error/404.html`);
+  res.sendFile(`${__dirname}/static/error/404.html`);
 });
 
 //Start server
